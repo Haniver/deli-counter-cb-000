c2fec59d2426d417f0e1391ab696fa168d4c5b5b
Lucio Ernesto Gutierrez Munch <luciogm@gmail.com>
2018-04-03 17:16:29 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,8 @@
-# Write your code here.
+def line(line_array)
+  output ="The line is currently: "
+  counter = 1
+  line_array.each do |person|
+    output += counter + ". " + person
+  end
+  output
+end

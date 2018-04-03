beb5b3b5a060ffa9cdb6b1976d330509d905e1a7
Lucio Ernesto Gutierrez Munch <luciogm@gmail.com>
2018-04-03 17:19:00 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,8 +1,11 @@
 def line(line_array)
-  output ="The line is currently: "
-  counter = 1
-  line_array.each do |person|
-    output += counter + ". " + person
-  end
-  output
+  if line_array.count > 0
+    output ="The line is currently: "
+    counter = 1
+    line_array.each do |person|
+      output += "#{counter}. #{person} "
+    end
+    puts output
+  else
+    puts "The line is currently empty."
 end
